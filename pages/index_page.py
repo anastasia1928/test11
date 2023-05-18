852e27b3787740bb7ce16b833b3600ffb46bf247
Belka <kuligina.anasttasia07@gmail.com>
2023-05-18 14:18:34 +0300

    creating project structure

--- pages/index_page.py
+++ pages/index_page.py
@@ -3,7 +3,14 @@ import config
 
 
 class IndexPage:
+    _BUTTON_GOOGlE_SEARCH = "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']"
+
 
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
-        return page.locator(self._BUTTON_ПОИСК_В_GOOGLE).get_attribute('value')
+
+
+
+
+        def open_index_page(self, page: Page) -> None:
+        return page.locator(self._BUTTON_GOOGlE_SEARCH).get_attribute('value')
